d8109a0d5817306a888a3b06ed133868025721f4
Andre Anjos <andre.dos.anjos@gmail.com>
2024-03-04 13:44:08 +0100

    Add gnuconfig requirement for build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 set -x -e
 
+# Debug: what is installed on the $BUILD_PREFIX with the current configuration?
+ls -Rl $BUILD_PREFIX
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
 # TODO
 # - Would be good if luarocks would offer the ability to use only a user rockspec, instead of system.
 #   (Using system-only rockspec with --force-config flag installs packages with restrictive privileges)
 ./configure --prefix=$PREFIX \
     --sysconfdir=$PREFIX/share/lua/ \
     --with-lua-include=$PREFIX/include \
-    --with-lua-interpreter=$BUILD_PREFIX/bin/lua \
     --rocks-tree=$PREFIX
 
 make bootstrap
