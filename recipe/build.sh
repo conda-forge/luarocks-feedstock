fa14e26571a86fa86d78ec45891ed138a1e8ad03
Andre Anjos <andre.dos.anjos@gmail.com>
2024-03-04 07:56:49 +0100

    Set interpreter so build works on cross-builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,7 @@ set -x -e
 ./configure --prefix=$PREFIX \
     --sysconfdir=$PREFIX/share/lua/ \
     --with-lua-include=$PREFIX/include \
+    --with-lua-interpreter=$BUILD_PREFIX/bin/lua \
     --rocks-tree=$PREFIX
 
 make bootstrap
