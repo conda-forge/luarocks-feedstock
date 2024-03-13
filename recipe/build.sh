6c869f8da3ec6d73c5549e8b74ca879bea03b6f8
Andre Anjos <andre.dos.anjos@gmail.com>
2024-03-13 21:30:02 +0100

    Fix sed command to account for forward slashes

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,7 +20,7 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
     for binfile in $PREFIX/bin/*; do
         if [[ -f "$binfile" && ! "$(file --mime-type --brief -- "$binfile")" =~ ^text$ ]]
         then
-            sed -i '' "s/${BUILD_PREFIX}/${PREFIX}/g" $${binfile}
+            sed -i '' "s,${BUILD_PREFIX},${PREFIX},g" $${binfile}
         fi
     done
 fi
