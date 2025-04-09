390d977624fb0beb504af4efe0e1b9a8af28c9e4
Andre Anjos <andre.dos.anjos@gmail.com>
2025-04-09 13:35:36 +0200

    matrix: builds for lua51, lua54 (latest) and luajit and for a variety of platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 set -x -e
 
+# See what is set
+# env | sort
+
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
 # TODO
 # - Would be good if luarocks would offer the ability to use only a user rockspec, instead of system.
 #   (Using system-only rockspec with --force-config flag installs packages with restrictive privileges)
-./configure --prefix=$PREFIX \
-    --sysconfdir=$PREFIX/share/lua/ \
-    --with-lua-include=$PREFIX/include \
-    --rocks-tree=$PREFIX
+if [[ "$variant" == "luajit" ]]; then
+    ./configure --prefix=$PREFIX \
+        --sysconfdir=$PREFIX/share/lua/ \
+        --with-lua-include=$(realpath $PREFIX/include/luajit-*) \
+        --rocks-tree=$PREFIX
+else
+    ./configure --prefix=$PREFIX \
+        --sysconfdir=$PREFIX/share/lua/ \
+        --with-lua-include=$PREFIX/include \
+        --rocks-tree=$PREFIX
+fi
 
 make bootstrap
 
@@ -18,6 +28,6 @@ make bootstrap
 # if we are cross compiling the package
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
     for binfile in $PREFIX/bin/luarocks{,-admin}; do
-        sed -i '' "s,${BUILD_PREFIX},${PREFIX},g" "${binfile}"
+        sed -i'' "s,${BUILD_PREFIX},${PREFIX},g" "${binfile}"
     done
 fi
